8f0c6450b33055bde5015dd6dacd78d83f9242d3
Chelsea Stamm <stamm.chelsea@gmail.com>
2019-10-23 19:33:08 -0600

    more JS updates

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,50 @@
-import { films } from '/JS/assets/films.js'
+import { planets } from '/JS/assets/planets.js'
+import { species } from '/JS/assets/species.js'
+
+let mainArea = document.querySelector('main')
+
+planets.forEach(planet => {
+    let planetDiv = document.createElement('div')
+    let name = document.createElement('h1')
+    let climate = document.createElement('h2')
+    let terrain = document.createElement('p')
+    let pic = document.createElement('img')
+    let planetNum = getPlanetNumber(planet.url)
+
+    name.textContent = planet.name
+    climate.textContent = planet.climate
+    terrain.textContent = planet.terrain
+
+    pic.src = `https://starwars-visualguide.com/assets/img/planets/${planetNum}.jpg`
+    pic.src = `https://starwars-visualguide.com/assets/img/placeholder.jpg`
+
+    planetDiv.appendChild(name)
+    planetDiv.appendChild(climate)
+    planetDiv.appendChild(terrain)
+
+    mainArea.appendChild(planetDiv)
+});
+
+
+function getPlanetNumber(planetURL) {
+    let end = planetURL.lastIndexOf ('/')
+    let planetID = planetURL.substring(end -2, end)
+    if(planetID.indexOf('/') !== -1) {
+        return planetID.slice(1,2)
+    } else {
+        return planetID 
+    }
+} 
+
+
+
+
+/* import { films } from '/JS/assets/films.js'
 import { people } from '/JS/assets/people.js'
 
 let mainArea = document.querySelector('main')
 
-/* films.forEach(function(film) {
+films.forEach(function(film) {
     let filmDiv = document.createElement('div')
     let filmTitle = document.createElement('h1')
     let filmCrawl = document.createElement('p')
@@ -15,7 +56,7 @@ let mainArea = document.querySelector('main')
     filmDiv.appendChild(filmCrawl)
 
     mainArea.appendChild(filmDiv)
-}); */
+}); 
 
 people.forEach((person) => {
     let personDiv = document.createElement('div')
@@ -77,5 +118,6 @@ femaleButton.addEventListener('click', () => {
 const maleCharacters = people.filter(person => person.gender === 'male')
 const femaleCharacters = people.filter(person => person.gender === 'female')
 const otherCharacters = people.filter(person => person.gender !== 'female' & 'male')
+ 
 
-
+ */
