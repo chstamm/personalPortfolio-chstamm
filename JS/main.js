4fb240eb74f1880bbe3acbd2dc53db8d60ab6672
Chelsea Stamm <stamm.chelsea@gmail.com>
2019-10-24 19:29:20 -0600

    Updates to images and new files

--- JS/main.js
+++ JS/main.js
@@ -3,28 +3,34 @@ import { species } from '/JS/assets/species.js'
 
 let mainArea = document.querySelector('main')
 
+/* This is a loop function */
 planets.forEach(planet => {
     let planetDiv = document.createElement('div')
     let name = document.createElement('h1')
-    let climate = document.createElement('h2')
+    let climate = document.createElement('p')
     let terrain = document.createElement('p')
     let pic = document.createElement('img')
     let planetNum = getPlanetNumber(planet.url)
 
+    pic.setAttribute('class', 'picDivs') /* You can now style it in css with .picDivs */
+    
     name.textContent = planet.name
-    climate.textContent = planet.climate
-    terrain.textContent = planet.terrain
+    climate.textContent = `Climate: ${planet.climate}`
+    terrain.textContent = `Terrain: ${planet.terrain}`
 
     pic.src = `https://starwars-visualguide.com/assets/img/planets/${planetNum}.jpg`
-    pic.src = `https://starwars-visualguide.com/assets/img/placeholder.jpg`
+    pic.addEventListener('error', (event) => {
+        let badImage = event.target
+        badImage.src = '../images/placeholder.jpg'
+    })
 
     planetDiv.appendChild(name)
     planetDiv.appendChild(climate)
     planetDiv.appendChild(terrain)
+    planetDiv.appendChild(pic)
 
     mainArea.appendChild(planetDiv)
 });
-
 
 function getPlanetNumber(planetURL) {
     let end = planetURL.lastIndexOf ('/')
@@ -36,88 +42,50 @@ function getPlanetNumber(planetURL) {
     }
 } 
 
-
-
-
-/* import { films } from '/JS/assets/films.js'
-import { people } from '/JS/assets/people.js'
-
-let mainArea = document.querySelector('main')
-
-films.forEach(function(film) {
-    let filmDiv = document.createElement('div')
-    let filmTitle = document.createElement('h1')
-    let filmCrawl = document.createElement('p')
-
-    filmTitle.textContent = film.title 
-    filmCrawl.textContent = film.opening_crawl
-
-    filmDiv.appendChild(filmTitle)
-    filmDiv.appendChild(filmCrawl)
-
-    mainArea.appendChild(filmDiv)
-}); 
-
-people.forEach((person) => {
-    let personDiv = document.createElement('div')
-    let name = document.createElement('h1')
-    let gender = document.createElement('p')
-    let pic = document.createElement('img')
-
-    let charNum = getCharNumber(person.url)
-
-    name.textContent = person.name
-    gender.textContent = person.gender
-    pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-
-    personDiv.appendChild(name)
-    personDiv.appendChild(gender)
-    personDiv.appendChild(pic)
-
-    mainArea.appendChild(personDiv)
-})
-
-function getCharNumber(charURL) {
-    let end = charURL.lastIndexOf ('/')
-    let charID = charURL.substring(end -2, end)
-    if(charID.indexOf('/') !== -1) {
-        return charID.slice(1,2)
-    } else {
-        return charID
-    }
-}
-
 const allDivs = Array.from(document.querySelectorAll('div'))
 const mainHeader = document.querySelector('header')
-let maleButton = document.createElement('button')
-maleButton.textContent = 'Male Characters'
+let tempButton = document.createElement('button')
+tempButton.textContent = 'Temperate'
+let tropButton = document.createElement('button')
+tropButton.textContent = 'Tropical Climate'
+mainHeader.appendChild(tempButton)
+mainHeader.appendChild(tropButton)
 
-maleButton.addEventListener('click', () => {
-    femaleCharacters.forEach(character => {
+
+
+tempButton.addEventListener('click', () => {
+    tropPlanet.forEach(planets => {
         let matchedDiv = allDivs.find(oneDiv => {
-            return oneDiv.firstChild.textContent === character.name
+            return oneDiv.firstChild.textContent === planets.climate
         })
         matchedDiv.setAttribute("style", "display: none;")
     })
 })
 
-let femaleButton = document.createElement('button')
-femaleButton.textContent = 'Female Characters'
-mainHeader.appendChild(maleButton)
-mainHeader.appendChild(femaleButton)
-
-femaleButton.addEventListener('click', () => {
-    maleCharacters.forEach(character => {
+tropButton.addEventListener('click', () => {
+    tempPlanet.forEach(planets => {
         let matchedDiv = allDivs.find(oneDiv => {
-            return oneDiv.firstChild.textContent === character.name
-        })
+            return oneDiv.firstChild.textContent === planets.climate
+        }) 
         matchedDiv.setAttribute("style", "display: none;")
     })
-})
+})  
 
-const maleCharacters = people.filter(person => person.gender === 'male')
-const femaleCharacters = people.filter(person => person.gender === 'female')
-const otherCharacters = people.filter(person => person.gender !== 'female' & 'male')
- 
+const tempPlanet = planets.filter(planet => planet.climate === 'temperate')
+const tropPlanet = planets.filter(planet => planet.climate === 'tropical')
 
- */
+console.log(tempPlanet)
+console.log(tropPlanet)
+/* const otherCharacters = planets.filter(planets => planets.climate !== 'temporate' & 'tropical')  */
+
+
+/* const tempPlanet = planets.filter(planet => planet.climate === 'hot')
+const tempClimate = planets.filter(planet => planet.climate === 'frigid')
+const tempClimate = planets.filter(planet => planet.climate === 'polluted')
+const tempClimate = planets.filter(planet => planet.climate === 'arid')
+const tempClimate = planets.filter(planet => planet.climate === 'superheated')
+const tempClimate = planets.filter(planet => planet.climate === 'unknown') */
+
+
+
+
