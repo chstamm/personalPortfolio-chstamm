b000c041bb2bc286b8a38f01bb791c9320caba27
Chelsea Stamm <stamm.chelsea@gmail.com>
2019-11-24 21:18:24 -0700

    some more updates

--- JS/pokemon.js
+++ JS/pokemon.js
@@ -13,7 +13,13 @@
      }
  }
 
+
  const random = new Pokemon(900, 'random') */
+
+
+ var pokeData = []
+ 
+ console.log(pokeData)
 
  const button = document.querySelector('#newPokemon')
  button.addEventListener('click', function(){
@@ -51,7 +57,7 @@ async function getAPIData(url) {
 }
 
 
-const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
+/* const theData = */ getAPIData('https://pokeapi.co/api/v2/pokemon/')
     .then(data => {
         for (const pokemon of data.results) {
             getAPIData(pokemon.url)
@@ -62,8 +68,7 @@ const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
         }
     })
 
-console.log(theData)
-
+ 
 // setting up the DOM
 
 let headerArea = document.querySelector('header')
@@ -125,7 +130,7 @@ function fillFront(frontInfo, data ) {
 function fillBack(backInfo, data) {
     let name = document.createElement('h1')
     let order = document.createElement('h1')
-    //let baseXP = document.createElement('p')
+    let baseXP = document.createElement('p')
     let HP = document.createElement('p')
     let speed = document.createElement('p')
     let attack = document.createElement('p')
@@ -133,16 +138,15 @@ function fillBack(backInfo, data) {
 
 
     order.textContent = `#${data.id} ${data.name[0].toUpperCase()}${data.name.slice(1)}`
-    //baseXP.textContent = `Base XP: ${data.abilities[2]}`
+    baseXP.textContent = `Base XP: ${data.base_experience}`
     HP.textContent = `HP: ${data.stats[5].base_stat}`
-    //HP.textContent = data.hp
     speed.textContent = `Speed: ${data.stats[0].base_stat}`
     attack.textContent = `Attack: ${data.stats[4].base_stat}`
     defence.textContent = `Defence: ${data.stats[3].base_stat}`
 
     backInfo.appendChild(order)
     backInfo.appendChild(name)
-    //backInfo.appendChild(baseXP)
+    backInfo.appendChild(baseXP)
     backInfo.appendChild(HP)
     backInfo.appendChild(speed)
     backInfo.appendChild(attack)
@@ -159,9 +163,3 @@ function getPokeNumber(id) {
     } else return id
 }
 
-/* function nameUpper(name) {
-   return name.charAt(0).toUpperCase() + name.slice(1); 
-   
-} */
-
- //how do I do this while being in the promise?
