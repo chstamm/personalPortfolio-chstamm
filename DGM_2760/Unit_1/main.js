3f4ed16a14f59832dfd0693e9cf3bb7467b2f856
Chelsea Stamm <stamm.chelsea@gmail.com>
2020-04-08 17:02:27 -0600

    Finishing Touches

--- DGM_2760/Unit_1/main.js
+++ DGM_2760/Unit_1/main.js
@@ -1,2 +1,10 @@
 document.querySelector('#name').innerText = "Joe's Bed and Breakfast"
 document.querySelector('#slogan').innerText = 'The Best Around!'
+
+
+
+let custName = prompt("Can you please give me your name?")
+
+let welcomeMessage = `Hello ${custName}, welcome to the Lodge!`
+
+document.querySelector ('#welcome').innerText = welcomeMessage
