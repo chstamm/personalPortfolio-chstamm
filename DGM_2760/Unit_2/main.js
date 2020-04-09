46e19b3e7d999c3c659fcf58acba6bf507a3cd98
Chelsea Stamm <stamm.chelsea@gmail.com>
2020-04-09 00:13:34 -0600

    JS updates

---
+++ DGM_2760/Unit_2/main.js
@@ -0,0 +1,10 @@
+document.querySelector('#name').innerText = "Inspector Panda"
+document.querySelector('#slogan').innerText = "Keeping Pandamonium to a Minimum"
+
+let windowSizes = `My window size is currently ${window.innerWidth} by ${window.innerHeight}.`
+
+let windowInfo = document.querySelector('#windowInfo').innerText = windowSizes
+
+let windowOffset = `My window is offset by `
+
+let offsetInfo = document.querySelector('#offsetInfo').innerText = windowOffset
