151e66ca4b9232a5f9a2ba8e14be8d46a35e587f
Chelsea Stamm <stamm.chelsea@gmail.com>
2020-04-26 21:55:45 -0600

    Added Animations

--- DGM_2780/Spa/js/index.js
+++ DGM_2780/Spa/js/index.js
@@ -1,3 +1,22 @@
 $('.carousel').carousel({
     interval: 8000,
 })
+
+gsap.from('.logo', {duration: 2, opacity: 0, y: 20});
+
+let logoImg = document.getElementById("logoImg");
+
+logoImg.addEventListener("click", function(event){
+    gsap.from(logoImg, {duration: 1, rotate: 360})
+});
+
+let imgTiles = document.getElementsByClassName("imgTile1");
+
+for (let imgTile of imgTiles){
+    imgTile.addEventListener("mouseenter", function(event){
+    gsap.to(imgTile, {duration: .5, scale: 1.2});
+    imgTile.addEventListener("mouseleave", function(event){
+    gsap.to(imgTile, {duration: .5, scale: 1});
+    });
+});}
+
