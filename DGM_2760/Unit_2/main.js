56fc52070528ef9c98210be7d9aac29d984e8d6c
Chelsea Stamm <stamm.chelsea@gmail.com>
2020-09-18 14:06:53 -0600

    Updates to Window  Poeperties

--- DGM_2760/Unit_2/main.js
+++ DGM_2760/Unit_2/main.js
@@ -1,10 +1,15 @@
 document.querySelector('#name').innerText = "Inspector Panda"
 document.querySelector('#slogan').innerText = "Keeping Pandamonium to a Minimum"
 
-let windowSizes = `My window size is currently ${window.innerWidth} by ${window.innerHeight}.`
 
-let windowInfo = document.querySelector('#windowInfo').innerText = windowSizes
+const watchWindow = () => {
+    let windowSize = `My window size is currently ${window.innerWidth} by ${window.innerHeight}.`
 
-let windowOffset = `My window is offset by `
+    let windowOffset = `My window is offset by ${window.screenX} from the left, and ${window.screenY} from the top.`
 
-let offsetInfo = document.querySelector('#offsetInfo').innerText = windowOffset
+    let windowInfo = document.querySelector('#windowInfo').innerText = `${windowSize}
+${windowOffset}`
+}
+
+watchWindow()
+
