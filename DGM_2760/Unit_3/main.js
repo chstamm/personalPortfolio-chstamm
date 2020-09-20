e80fa949af4b6373918a5961cb72d94925b279fc
Chelsea Stamm <stamm.chelsea@gmail.com>
2020-09-19 21:10:30 -0600

    Finished Up Stuff

--- DGM_2760/Unit_3/main.js
+++ DGM_2760/Unit_3/main.js
@@ -9,8 +9,6 @@ let randNum = Math.floor(Math.random() * (1 - 16) + 16);
 
 console.log(randNum);
 
-//let randNum = Math.floor(Math.random() * 16)
-
 let guessed = false;
 let totalGuesses = 0;
 let userGuess = 0;
@@ -19,12 +17,16 @@ function guessEval() {
   totalGuesses++;
   userGuess = document.querySelector("#guess").value;
   document.querySelector("#tries").innerText = totalGuesses;
-  
-
   let feedback = document.querySelector("#feedback");
+  let rewardImg = document.querySelector("#rewardImg");
+
+  while (rewardImg.firstChild) {
+    rewardImg.removeChild(rewardImg.firstChild);
+  }
 
   if (userGuess == randNum) {
     feedback.innerText = "Correctomundo!";
+    award();
   } else if (userGuess > randNum && userGuess < 16) {
     feedback.innerText = "Too High!";
   } else if (userGuess < randNum && userGuess > 0) {
@@ -35,8 +37,26 @@ function guessEval() {
   }
 }
 
-/* function award () {
-   case 1: 
-   case 2:
-   case 3: 
-} */
+function displayImg(url) {
+  let rewardImg = document.querySelector("#rewardImg");
+  let ribbon = document.createElement("img");
+  ribbon.src = url;
+  rewardImg.appendChild(ribbon);
+}
+
+function award() {
+  switch (totalGuesses) {
+    case 1:
+    case 2:
+    case 3:
+      displayImg("./Images/blue_ribbon.png");
+      break;
+    case 4:
+    case 5:
+    case 6:
+      displayImg("./Images/red_ribbon.png");
+      break;
+    default:
+      displayImg("./Images/yellow_ribbon.png");
+  }
+}
