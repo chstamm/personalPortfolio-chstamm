c62ffaf757326d0cec07fd8a35900e2cb14a2b67
Chelsea Stamm <stamm.chelsea@gmail.com>
2020-10-21 22:31:25 -0600

    finished project

--- DGM_2760/Unit_5/main.js
+++ DGM_2760/Unit_5/main.js
@@ -9,8 +9,78 @@ const results = document.querySelector("#results");
 const displayTrees = () => {
   let treeList = "";
   trees.forEach((tree) => {
-      console.log(tree)
+    treeList += `${tree} <br>`;
   });
+  results.innerHTML =
+    treeList + `<span> This list is ${trees.length} elements long </span>`;
+  //console.log(treeList)
 };
 
-displayTrees()
+displayTrees();
+
+document.getElementById("add-cherry").onclick = () => {
+  trees.push("Cherry");
+  displayTrees();
+};
+
+document.getElementById("add-orange").onclick = () => {
+  trees.unshift("Orange");
+  displayTrees();
+};
+
+document.getElementById("remove-first").onclick = () => {
+  if (trees.length > 1) {
+    trees.shift();
+    displayTrees();
+  } else {
+    error.textContent = "Sorry, Can't remove last Tree!";
+  }
+};
+
+document.getElementById("remove-second").onclick = () => {
+  if (trees.length > 1) {
+    trees.splice(1, 1);
+    displayTrees();
+  } else {
+    error.textContent = "Sorry, Can't remove last tree!";
+  }
+};
+
+document.getElementById("remove-end").onclick = () => {
+  if (trees.length > 1) {
+    trees.pop();
+    displayTrees();
+  } else {
+    error.textContent = "Sorry, Can't remove last Tree!";
+  }
+};
+
+document.getElementById("sortTrees").onclick = () => {
+  trees.sort();
+  displayTrees();
+};
+
+document.getElementById("lowerCase").onclick = () => {
+  trees.forEach((treeLowerCase, index, array) => {
+    array[index] = treeLowerCase.toLowerCase();
+  });
+  displayTrees();
+};
+
+document.getElementById("name3").onclick = () => {
+  if (trees.length > 2) {
+    const thirdTree = trees[2]
+    error.textContent = `The third tree is ${thirdTree}`;
+  } else {
+      error.textContent = `Sorry, there isn't a third tree.`
+  }
+};
+
+document.getElementById("name4").onclick = () => {
+    if (trees.length > 3) {
+      const fourthTree = trees[3]
+      error.textContent = `The fourth tree is ${fourthTree}.`;
+    } else {
+        error.textContent = `Sorry, there isn't a fourth tree.`
+    }
+  };
