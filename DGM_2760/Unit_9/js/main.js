b6989f3d5c0d0e12e0edd3fc002414da40a35711
Chelsea Stamm <stamm.chelsea@gmail.com>
2020-11-27 19:02:09 -0700

    Finished Project

--- DGM_2760/Unit_9/js/main.js
+++ DGM_2760/Unit_9/js/main.js
@@ -1,20 +1,25 @@
 const duplicateMenu = () => {
-    //get all of the anchor elements from the top menu
-    //querySelectorAll
     let topMenu = document.querySelectorAll('ul#primaryNavigation li a')
-    console.log(topMenu)
 
     //create the new list items for the button of the page 
-    let newList = document.createElement('ul')
+    
+    let newMenu = document.querySelector('#smallNavArea')
+
 
     //use forEach
     topMenu.forEach(menuItem => {
+        let newList = document.createElement('ul')
         let newItem = document.createElement('li')
         let newLink = document.createElement('a')
         newLink.setAttribute('href',menuItem.getAttribute('href'))
+        newLink.innerText = menuItem.innerText
+        newList.appendChild(newItem)
+        newItem.appendChild(newLink)
+        newMenu.appendChild(newList)
+
 
         //Copy the text content from upper menu to new menu
-        //append children to make tem appear in the DOM. 
+        //append children to make them appear in the DOM. 
     })
 }
 duplicateMenu()
