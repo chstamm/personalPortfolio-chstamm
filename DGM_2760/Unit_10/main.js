4c4cbdde825d74eba59eee6aa752c239c4b840cf
Chelsea Stamm <stamm.chelsea@gmail.com>
2020-11-28 20:10:53 -0700

    Added event listeners and got data to display

--- DGM_2760/Unit_10/main.js
+++ DGM_2760/Unit_10/main.js
@@ -1,3 +1,36 @@
 document.querySelector("#title").innerText = "Hotel Finder";
 document.querySelector("#slogan").innerText = "Find Your Happy Place";
 
+async function getData() {
+  try {
+    const response = await fetch("hotels.json");
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+let hotelData = {};
+
+getData().then(data => hotelData = data);
+let links = document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener('click', hotelInfo)
+  
+})
+
+
+function hotelInfo(event) { 
+  let hotelChosen = hotelData.hotels.find(hotels => {
+    return event.target.id === hotels.name.toLowerCase()
+  })
+  console.log(hotelChosen)
+
+  document.querySelector("#hotelName").textContent = `${hotelChosen.name} Hotel`
+  document.querySelector("#address").textContent = `${hotelChosen.address}`
+  document.querySelector("#rooms").textContent = `${hotelChosen.rooms}`
+  document.querySelector("#gym").textContent = `${hotelChosen.gym}`
+  document.querySelector("#type").textContent = `${hotelChosen.roomTypes.join(", ")}`
+  document.querySelector("#picture").src = `imgs/${hotelChosen.picture}`
+}
+
+
